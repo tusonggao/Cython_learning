6fd733c925acb933780749ea623990bbe3be9717
tusonggao <tusonggao@163.com>
2020-01-19 11:39:38 +0800

    add files

---
+++ faster_model_with_cython.py
@@ -0,0 +1,11 @@
+# https://github.com/huggingface/100-times-faster-nlp
+from __future__ import print_function, division, with_statement
+import os
+import sys
+import time
+import numpy as np
+import pandas as pd
+
+if __name__=='__main__':
+    print('hello world')
+
